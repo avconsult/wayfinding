71ec59f6c75dfd4962020ea129d309ae78a60e9a
mozein15 <mozein15@gmail.com>
2021-11-01 04:57:23 -0700

    Added keyboard files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const imageMapContainer = document.getElementById("map-container");
 const imageMap = document.getElementById("my-map");
 const videoPlayer = document.getElementById("video-player");
+const threeDview = document.getElementById('modelo-wrapper');
 
 function playvideo(){
     imageMap.classList.add('animate__backOutLeft');
@@ -11,4 +12,16 @@ function playvideo(){
     setTimeout(function () {
             videoPlayer.play();
         }, 500);
+}
+
+function toggle3D(){
+    console.log( window.getComputedStyle(imageMapContainer, null).display )
+    if( window.getComputedStyle(imageMapContainer, null).display == 'none' ){
+        imageMapContainer.style.display = 'flex';
+        threeDview.style.display = 'none';
+    } else {
+        imageMapContainer.style.display = 'none';
+        threeDview.style.display = 'block';
+    }
+
 }
