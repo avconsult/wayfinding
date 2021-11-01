58497519c7d4fd1ebd15596a8c2a47ebffb0f2d5
mozein15 <mozein15@gmail.com>
2021-11-01 15:06:41 -0700

    Added exit button from video

--- app.js
+++ app.js
@@ -2,26 +2,36 @@
 const imageMapContainer = document.getElementById("map-container");
 const imageMap = document.getElementById("my-map");
 const videoPlayer = document.getElementById("video-player");
-const threeDview = document.getElementById('modelo-wrapper');
+const closeVideoButton = document.getElementById("exit-video");
 
 function playvideo(){
-    imageMap.classList.add('animate__backOutLeft');
     videoPlayer.style.display = 'block';
     videoPlayer.classList.add('animate__backInRight');
     imageMap.style.display = 'none';
     setTimeout(function () {
             videoPlayer.play();
         }, 500);
+    setTimeout(function () {
+       closeVideoButton.style.visibility = 'visible';
+    }, 1000);
 }
 
-function toggle3D(){
-    console.log( window.getComputedStyle(imageMapContainer, null).display )
-    if( window.getComputedStyle(imageMapContainer, null).display == 'none' ){
-        imageMapContainer.style.display = 'flex';
-        threeDview.style.display = 'none';
-    } else {
-        imageMapContainer.style.display = 'none';
-        threeDview.style.display = 'block';
-    }
-
+function closeVideo(){
+    closeVideoButton.style.visibility = 'hidden';
+    videoPlayer.style.display = 'none';
+    imageMap.style.display = 'flex';
+    imageMap.classList.add('animate__backInLeft');
 }
+
+
+// function toggle3D(){
+//     console.log( window.getComputedStyle(imageMapContainer, null).display )
+//     if( window.getComputedStyle(imageMapContainer, null).display == 'none' ){
+//         imageMapContainer.style.display = 'flex';
+//         threeDview.style.display = 'none';
+//     } else {
+//         imageMapContainer.style.display = 'none';
+//         threeDview.style.display = 'block';
+//     }
+//
+// }
