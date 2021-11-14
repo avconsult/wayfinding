42b783e8945cfade90dd36d7083f47127fbb0868
mozein15 <mozein15@gmail.com>
2021-11-14 12:41:57 -0800

    Added bottom toolbar

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const videoPlayer = document.getElementById("video-player");
 const closeVideoButton = document.getElementById("exit-video");
 const tapGesture = document.getElementById("touch-here");
 const poiGrid = document.getElementById("destinations-grid");
+const langList = document.getElementById("language-list");
+const englishDiv = langList.firstElementChild;
+const arabicDiv = langList.childNodes[3];
+console.log(arabicDiv);
 
 function playvideo(){
     videoPlayer.style.display = 'block';
@@ -36,6 +40,24 @@ function resetTapGesture(){
 
 function showPoiGrid(){
     poiGrid.style.display = 'grid';
+    langList.style.display = 'none';
     poiGrid.classList.add("animate__fadeInUp");
-    console.log('clicked POI');
+}
+
+function showLanguages(){
+    poiGrid.style.display = 'none';
+    langList.style.display = 'flex';
+    langList.classList.add("animate__fadeInUp");
+}
+
+function changeToArabic(){
+    englishDiv.classList.remove('selected');
+    englishDiv.getElementsByTagName('span')[0].textContent = '';
+    englishDiv.getElementsByTagName('span')[0].classList.remove('check');
+    arabicDiv.classList.add('selected');
+    arabicDiv.getElementsByTagName('span')[0].textContent = '✔';
+    arabicDiv.getElementsByTagName('span')[0].classList.add('check');
+    setTimeout(function () {
+        window.location.href = "arabic/arabindex.html";
+    }, 100)
 }
